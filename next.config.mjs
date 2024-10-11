380c2a446ab6670cf961f89520001d035c1c56b7
anshjain321 <136158147+anshjain321@users.noreply.github.com>
2024-10-11 13:21:12 +0530

    deploy error fixed

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
       domains: ['iconscout.com'],
     },
+    experimental: {
+      missingSuspenseWithCSRBailout: false,
+    },
   };
   
 export default nextConfig;
